b76580d24bfaee8fe897a587f569adbd5b40971b
Takuma Osada <judgment9ttk069@gmail.com>
2024-06-13 15:19:53 +0900

    initial

---
+++ sli_slo/lib/sli_slo.dart
@@ -0,0 +1,7 @@
+library sli_slo;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ sli_slo/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/s11080/.asdf/installs/flutter/3.7.12-stable"
+export "FLUTTER_APPLICATION_PATH=/Users/s11080/workspace/takumaosada/sli-slo-flutter-example/sli_slo"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
